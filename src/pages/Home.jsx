658490b752d34d0b9f2bfc99d3f624e3844b8f32
delta student <yashparadkar63@gmail.com>
2026-07-21 14:54:48 +0530

    feat: Remove Shop By Occasion section from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Hero from '../components/Hero';
 import OurStory from '../components/OurStory';
 import Collections from '../components/Collections';
-import ShopByOccasion from '../components/ShopByOccasion';
+
 import CorePillars from '../components/CorePillars';
 import Craftsmanship from '../components/Craftsmanship';
 import RealBrides from '../components/RealBrides';
@@ -12,7 +12,7 @@ const Home = () => {
       <Hero />
       <OurStory />
       <Collections />
-      <ShopByOccasion />
+
       <Craftsmanship />
       <RealBrides />
       <CorePillars />
